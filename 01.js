519042cdd83ad5791f2a7e6782116caa7385580d
Jhovan Palafox <Mixcoalt>
2023-07-18 16:34:00 -0600

    E-commerce demo

---
+++ 01.js
@@ -0,0 +1,51 @@
+const solution = (year) => {
+    if(year >= 1 && year <= 100){
+        console.log(1)
+    }else if(year >= 101 && year <= 200){
+        console.log(2)
+    }else if(year >= 201 && year <= 300){
+        console.log(3)
+    }else if(year >= 301 && year <= 400){
+        console.log(4)
+    }else if(year >= 401 && year <= 500){
+        console.log(5)
+    }else if(year >= 501 && year <= 600){
+        console.log(6)
+    }else if(year >= 601 && year <= 700){
+        console.log(7)
+    }else if(year >= 701 && year <= 800){
+        console.log(8)
+    }else if(year >= 801 && year <= 900){
+        console.log(9)
+    }else if(year >= 901 && year <= 1000){
+        console.log(10)
+    }else if(year >= 1001 && year <= 1100){
+        console.log(11)
+    }else if(year >= 1101 && year <= 1200){
+        console.log(12)
+    }else if(year >= 1201 && year <= 1300){
+        console.log(13)
+    }else if(year >= 1301 && year <= 1400){
+        console.log(14)
+    }else if(year >= 1401 && year <= 1500){
+        console.log(15)
+    }else if(year >= 1501 && year <= 1600){
+        console.log(16)
+    }else if(year >= 1601 && year <= 1700){
+        console.log(17)
+    }else if(year >= 1701 && year <= 1800){
+        console.log(18)
+    }else if(year >= 1801 && year <= 1900){
+        console.log(19)
+    }else if(year >= 1905 && year <= 2000){
+        console.log(20)
+    }else if(year >= 2001 && year <= 2100){
+        console.log(21)
+    }
+    else{
+        return 
+    }
+    return year
+}
+
+solution(2023)
